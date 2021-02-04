2e2bcf81269b9124d8edcda6125e93a160a08a62
repair-cat <simf61@yandex.ru>
2021-02-04 13:54:23 +0500

    добавлена форма вывода плана меню

--- inkitchen/order/forms.py
+++ inkitchen/order/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Order, OrderItem
+from django.forms import Select, RadioSelect
 
 
 class OrderItemForm(forms.ModelForm, forms.Form):
@@ -8,7 +9,22 @@ class OrderItemForm(forms.ModelForm, forms.Form):
         fields = ['product', ]
 
         widgets = {
-            'product': forms.RadioSelect(attrs={
+            'product': Select(attrs={
                 'class': 'select-product',
+                'style': 'width: 200px',
             }),
         }
+
+
+class PlanMenuForm(forms.Form):
+    """
+    План меню для выбора количества блюд на главной странице
+    """
+    PLAN_MENU = (
+        ('4', 4),
+        ('6', 6),
+        ('8', 8),
+        ('10', 10)
+    )
+
+    plan_menu = forms.ChoiceField(choices=PLAN_MENU, widget=RadioSelect(attrs={'class': 'card-radio'}), initial='4')
