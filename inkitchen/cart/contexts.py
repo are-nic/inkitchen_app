676cd8038e490c6d6ab7065332aefa3ac1aa7109
repair-cat <simf61@yandex.ru>
2021-04-05 12:04:22 +0500

    изменена логика подсчета стоимости и кол-ва рецептов в корзине

--- inkitchen/cart/contexts.py
+++ inkitchen/cart/contexts.py
@@ -1,12 +1,11 @@
 from django.shortcuts import get_object_or_404
-from food.models import Recipe, Product
+from food.models import Recipe
 
 
 def cart_contents(request):
     """
-    Подсчет стоимости рецептов и количества их порций и ингридиентов.
-    Общая стоимость рецептов складывается из стоимости первого продукта в списке ингридиента, кол-ва ингридиентов
-    и количества порций.
+    Подсчет стоимости рецептов и количества их порций.
+    Общая стоимость рецептов складывается из стоимости всех рецептов в корзине и количества порций.
     Обеспечивает доступность содержимого корзины в navbar при рендеринге каждой страницы
     """
     cart = request.session.get('cart', {})
@@ -16,21 +15,13 @@ def cart_contents(request):
     recipe_count = 0        # общее количество рецептов
     
     for id, q_portion in cart.items():
-        recipe_price = 0                           # стоимость каждого рецепта в корзине исходя из стоимости инридиентов
-        recipe = get_object_or_404(Recipe, pk=id)   # получаем экземпляр рецепта, кот. добавляем в корзину
-        ingredients = recipe.ingredients.all()      # получаем все игредиенты текущего рецепта
-        for ingredient in ingredients:
-            # цена рецепта
-            recipe_price += q_portion * ingredient.qty * Product.objects.filter(tag=ingredient.name.id)[0].price
-
-            # общая цена
-            total += q_portion * ingredient.qty * Product.objects.filter(tag=ingredient.name.id)[0].price
+        recipe = get_object_or_404(Recipe, pk=id)   # получаем экземпляр рецепта, добавленый в корзину
+        total += q_portion * recipe.price           # цена заказа
         recipe_count += q_portion
         cart_items.append({
             'id': id,
             'q_portion': q_portion,
             'recipe': recipe,
-            'recipe_price': recipe_price
         })
     
     return {'cart_items': cart_items, 'total': total, 'recipe_count': recipe_count}
