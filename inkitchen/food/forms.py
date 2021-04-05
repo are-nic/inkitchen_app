4ca906af79e49d99c6a822997aaa186fefecb749
repair-cat <simf61@yandex.ru>
2021-04-05 12:05:34 +0500

    добавлено поле Цена

--- inkitchen/food/forms.py
+++ inkitchen/food/forms.py
@@ -1,4 +1,4 @@
-from .models import Recipe, IngredientOfRecipe, Product
+from .models import Recipe, Ingredient
 from django import forms
 from django.forms import (ModelForm,
                           TextInput,
@@ -7,19 +7,15 @@ from django.forms import (ModelForm,
                           FileInput,
                           CheckboxSelectMultiple,
                           NumberInput,
-                          RadioSelect
                           )
 
 
 class RecipeForm(ModelForm, forms.Form):
     """форма добавления рецепта"""
 
-    CHOICES = (('OFFICE', 'Офиса'), ('HOME', 'Дома'),)
-    group = forms.ChoiceField(choices=CHOICES, label='Группа рецепта', widget=forms.RadioSelect, initial='HOME')
-
     class Meta:
         model = Recipe
-        fields = ['category', 'title', 'description', 'cooking_time', 'image', 'group', 'tags']
+        fields = ['category', 'title', 'description', 'cooking_time', 'image', 'price', 'tags']
 
         # типы полей и их аттрибуты
         widgets = {
@@ -34,15 +30,17 @@ class RecipeForm(ModelForm, forms.Form):
                 'class': 'form-recipe-title form-control',
                 'placeholder': 'Описание рецепта',
             }),
+            'price': NumberInput(attrs={
+                'class': 'form-recipe-price form-control',
+                'style': 'width: 50px',
+                'step': '0.01'
+            }),
             'cooking_time': TextInput(attrs={
                 'class': 'form-recipe-cook-time form-control',
                 'placeholder': 'Время приготовления',
             }),
             'image': FileInput(attrs={
                 'class': 'form-recipe-image',
-            }),
-            'group': RadioSelect(attrs={
-                'class': 'form-recipe-group',
             }),
             'tags': CheckboxSelectMultiple(attrs={
                 'class': 'form-recipe-tags',
@@ -53,11 +51,11 @@ class RecipeForm(ModelForm, forms.Form):
 
 class IngredientForm(ModelForm, forms.Form):
     class Meta:
-        model = IngredientOfRecipe
+        model = Ingredient
         fields = '__all__'
 
 
-IngredientFormSet = forms.inlineformset_factory(Recipe, IngredientOfRecipe, form=IngredientForm, can_delete=True,
+IngredientFormSet = forms.inlineformset_factory(Recipe, Ingredient, form=IngredientForm, can_delete=True,
                                                 fields=('name', 'qty', 'unit'), max_num=100, min_num=1, extra=0,
                                                 fk_name='recipe',
                                                 widgets={
