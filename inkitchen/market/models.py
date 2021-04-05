dfa0ac8f7fc4997341c8eaecc20d71ded90fa1c3
repair-cat <simf61@yandex.ru>
2021-04-05 12:07:56 +0500

    переименование моделей. изменение полей

--- inkitchen/market/models.py
+++ inkitchen/market/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.shortcuts import reverse
-from food.models import TagIngredient
+from food.models import TagProduct
 
 
 class CategoryProduct(models.Model):
@@ -12,8 +12,8 @@ class CategoryProduct(models.Model):
 
     class Meta:
         ordering = ('name',)
-        verbose_name = 'Категория продуктов'
-        verbose_name_plural = 'Категории продуктов'
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
         db_table = 'CategoryProduct'
 
     def __str__(self):
@@ -27,19 +27,21 @@ class ProductMarket(models.Model):
     category = models.ForeignKey(CategoryProduct, related_name='products', verbose_name='Категория', on_delete=models.SET_NULL,
                                  null=True, blank=True)
     name = models.CharField(max_length=300, verbose_name='Наименование')
+    # qty_per_item = models.PositiveIntegerField(verbose_name='Кол-во на ед. продукта')
+    # unit = models.CharField(max_length=10, verbose_name='Ед. измерения')
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True, verbose_name='Фото')
     description = models.TextField(blank=True, verbose_name='Описание')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
     stock = models.PositiveIntegerField(verbose_name='Остаток')
-    tags = models.ManyToManyField(TagIngredient, blank=True, verbose_name='Тэги', related_name='products')
+    tags = models.ManyToManyField(TagProduct, blank=True, verbose_name='Тэги', related_name='products')
     available = models.BooleanField(default=True, verbose_name='В наличии')
     created = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
     updated = models.DateTimeField(auto_now=True, verbose_name='Обновлен')
 
     class Meta:
         ordering = ('name',)
-        verbose_name = 'Продукт магазина'
-        verbose_name_plural = 'Продукты магазина'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
         db_table = 'ProductMarket'
 
     def __str__(self):
