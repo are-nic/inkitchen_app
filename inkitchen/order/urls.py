29efa108c953c3722be450e586c2c0a32611f9de
repair-cat <simf61@yandex.ru>
2021-04-23 10:44:21 +0500

    добавлен путь к методу определения местоположения

--- inkitchen/order/urls.py
+++ inkitchen/order/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import order_create
+from .views import order_create, get_current_location
 
 urlpatterns = [
     path('create', order_create, name='order_create'),
+    path('get_current_location', get_current_location, name='get_current_location'),
 ]
