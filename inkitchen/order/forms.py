e0e5ed8e3454dbdf73afe8e5842804bd52ca168a
repair-cat <simf61@yandex.ru>
2021-04-23 10:09:34 +0500

    добавлены виджеты и новые поля в формы заказа

--- inkitchen/order/forms.py
+++ inkitchen/order/forms.py
@@ -1,14 +1,51 @@
 from django import forms
 from .models import Order
-from django.forms import RadioSelect
 from datetime import date, timedelta
 import locale
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
 
 class OrderForm(forms.ModelForm, forms.Form):
+
+    city = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        'class': 'form-order',
+        'placeholder': 'Город',
+    }))
+    street = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        'class': 'form-order',
+        'placeholder': 'Улица',
+    }))
+    building = forms.CharField(max_length=10, widget=forms.TextInput(attrs={
+        'class': 'form-order',
+        'placeholder': 'Дом',
+    }))
+    apartment = forms.IntegerField(required=False, min_value=1, widget=forms.NumberInput(attrs={
+        'class': 'form-order',
+        'placeholder': 'Квартира',
+    }))
+
     class Meta:
         model = Order
-        fields = []
+        fields = ['name', 'address', 'phone_number', 'city', 'street', 'building', 'apartment', 'pay_method', 'at_door']
+
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-order',
+                'placeholder': 'Имя',
+            }),
+            'address': forms.TextInput(attrs={
+                'class': 'form-order',
+                'placeholder': 'Адрес',
+            }),
+            'phone_number': PhoneNumberPrefixWidget(attrs={
+                'class': 'form-order',
+                'placeholder': 'Телефон',
+            }),
+            'at_door': forms.CheckboxInput(attrs={
+                'class': 'form-order',
+                'placeholder': 'Оставить заказ у дверей',
+            }),
+        }
 
 
 class PlanMenuForm(forms.Form):
@@ -26,4 +63,6 @@ class PlanMenuForm(forms.Form):
         key += 1
     key = 0
 
-    week = forms.ChoiceField(choices=WEEK, widget=RadioSelect(attrs={'class': 'card-radio'}))
+    delivery_date = forms.DateField()
+    delivery_time = forms.TimeField()
+    qty_meals = forms.NumberInput()
