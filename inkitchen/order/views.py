bca6775752ffa4613aae848b1cb6fb341c0c2370
repair-cat <simf61@yandex.ru>
2021-05-10 04:28:03 +0500

    .

--- inkitchen/order/views.py
+++ inkitchen/order/views.py
@@ -1,8 +1,10 @@
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
-from .forms import OrderForm
+from .forms import OrderForm, PlanMenuFormSet
 from .models import OrderRecipe, Order
 from django import forms
+from datetime import date, timedelta, time
+import locale
 
 from geopy.geocoders import Nominatim
 
