3dd02ec038a4b6edb1b162d4b2ed5e6104d156a0
repair-cat <simf61@yandex.ru>
2021-05-10 04:18:17 +0500

    добавлены роуты

--- inkitchen/food/urls.py
+++ inkitchen/food/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import all_recipes, create_recipe, RecipeDetailView, edit_recipe, remove_recipe
+from .views import all_recipes, create_recipe, RecipeDetailView, edit_recipe, remove_recipe, delivery_day
 
 
 urlpatterns = [
     path('', all_recipes, name='recipes'),
+    path('week/<slug:index>', delivery_day, name='delivery_day'),
     path('create', create_recipe, name='create_recipe'),
     path('edit/<id>', edit_recipe, name='edit_recipe'),
     path('remove/<id>', remove_recipe, name='remove_recipe'),
--- inkitchen/market/urls.py
+++ inkitchen/market/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import product_list
+from .views import product_list, update_item
 
 
 urlpatterns = [
     path('', product_list, name='product_list'),
     path('<slug:category_slug>', product_list, name='product_list_by_category'),
+    path('update_item/', update_item, name='update_item'),
 ]
