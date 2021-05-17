94a73e7ce28d06eb5e03f5a8f627e82fe578020d
repair-cat <simf61@yandex.ru>
2021-05-17 10:20:43 +0500

    убран id из роута, т.к. теперь работает через ajax

--- inkitchen/cart/urls.py
+++ inkitchen/cart/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import cart_add, adjust_cart, remove_recipe_from_cart, add_to_cart
+from .views import cart_add, adjust_cart, remove_from_cart, add_to_cart
 
 urlpatterns = [
-    path('add/<id>', cart_add, name='cart_add'),
-    path('add_to_cart/', add_to_cart, name='add_to_cart'),
-    path('adjust/<id>', adjust_cart, name='adjust_cart'),
-    path('remove/<id>', remove_recipe_from_cart, name='remove_from_cart'),
+    path('add/<id>', cart_add, name='cart_add'),            # используется везде, кроме страницы recipes
+    path('add_to_cart/', add_to_cart, name='add_to_cart'),  # исправить путь после удаление верхнего роута
+    path('adjust/', adjust_cart, name='adjust_cart'),
+    path('remove/', remove_from_cart, name='remove_from_cart'),
 ]
+
+'''
+from django.contrib.sessions.models import Session
+Session.objects.all()
+Session.objects.get(pk='7o5hrpck8nndmg0r8w71go82xiv63qpb').get_decoded()
+'''
