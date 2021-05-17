7634e4f364dbaa6f36ca408050807b36216e73e7
repair-cat <simf61@yandex.ru>
2021-05-17 10:53:17 +0500

    переделаны функции удаления товаров и обновления их кол-ва

--- inkitchen/cart/views.py
+++ inkitchen/cart/views.py
@@ -29,46 +29,60 @@ def add_to_cart(request):
     Данные передаются в функцию, которая проверят: было ли добавлено блюдо в корзину ранее или впервые.
     В первом случае к имеющемуся кол-ву порций прибавляется 1. Во втором случае кол-во порций устанавливается = 1
     """
-    recipe_id = request.GET.get('recipe_id', None)  # получаем из ajax id добавленного в корзину рецепта
+    recipe_id = request.GET.get('recipe_id', None)          # получаем из ajax id добавленного в корзину рецепта
+    delivery_date = request.GET.get('delivery_date', None)  # получаем из ajax дату заказа
 
     cart = request.session.get('cart', {})
-
-    # проверяем, есть ли рецепт в корзине (в сессии)
-    if recipe_id not in cart:                       # если рецепта нет в корзине, то кол-во порций устанавливаем = 1
+    if delivery_date not in cart:                           # если дата заказа еще не была в корзине,
+        cart[delivery_date] = {}                            # то заводим для нее пустой словарь
+    # проверяем, есть ли рецепт в корзине (в сессии) по конкретной дате заказа
+    if recipe_id not in cart[delivery_date]:           # если рецепта нет в корзине, то кол-во порций устанавливаем = 1
         quantity = 1
-        cart[recipe_id] = quantity                  # заносим в сессию кол-во порций данного блюда
-    else:                                           # если на момент добавления блюда, оно уже в корзине
-        quantity = cart[recipe_id] + 1              # увеличиваем имеющееся кол-во порций юлюда в корзине на 1
-        cart[recipe_id] = quantity
+        cart[delivery_date][recipe_id] = quantity           # заносим в сессию кол-во порций данного блюда
+    else:                                                   # если на момент добавления блюда, оно уже в корзине
+        quantity = cart[delivery_date][recipe_id] + 1       # увеличиваем имеющееся кол-во порций юлюда в корзине на 1
+        cart[delivery_date][recipe_id] = quantity
 
-    cart[recipe_id] = cart.get(recipe_id, quantity)
+    cart[delivery_date][recipe_id] = cart.get(recipe_id, quantity)
 
     request.session['cart'] = cart                  # обновляем корзину с учетом изменений
 
     return HttpResponse("рецепт добавлен")
 
 
-def adjust_cart(request, id):
+def adjust_cart(request):
     """
-    Отрегулировать кол-во блюд внутри корзины
+    Регулировать кол-во блюд внутри корзины
     """
-    quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
+    recipe_id = request.GET.get('recipe_id', None)
+    delivery_date = request.GET.get('delivery_date', None)
+    quantity = int(request.GET.get('quantity', None))
 
-    if quantity > 0:
-        cart[id] = quantity
-    else:
-        cart.pop(id)
+    if request.GET.get('quantity_plus'):            # если была нажата кнопка "+" - увеличение кол-ва порций
+        cart[delivery_date][recipe_id] += 1         # добавляем 1 к кол-ву порций конкретного рецепта в корзине
+    elif request.GET.get('quantity_minus'):         # если была нажата кнопка "-" - уменьшение кол-ва порций
+        cart[delivery_date][recipe_id] -= 1         # отнимаем 1 от кол-ва порций конкретного рецепта в корзине
+    elif quantity <= 0:                             # если кол-во порций рецепта в корзине <= 0...
+        cart[delivery_date].pop(recipe_id)          # ...удаляем выбранный рецепт из корзины в сессии
 
     request.session['cart'] = cart
-    return redirect(reverse('recipes'))
+    return HttpResponse("корзина изменена")
 
 
-def remove_recipe_from_cart(request, id):
+def remove_from_cart(request):
     """
     Удалить блюдо из корзины
     """
     cart = request.session.get('cart', {})
-    cart.pop(id)
+
+    recipe_id = request.GET.get('recipe_id', None)
+    delivery_date = request.GET.get('delivery_date', None)
+
+    cart[delivery_date].pop(recipe_id)
+
+    if len(cart[delivery_date]) == 0:       # если кол-во рецептов в конкретной дате корзины = 0
+        cart.pop(delivery_date)             # удаляем эту дату из корзины сессии
+
     request.session['cart'] = cart
-    return redirect(reverse('recipes'))
+    return HttpResponse("рецепт удален")
