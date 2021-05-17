254d2ea4998fafe15c702eb220f405635384a988
repair-cat <simf61@yandex.ru>
2021-05-17 10:30:00 +0500

    добавлены обработки двух кнопок по изменению кол-ва товара в корзине

--- inkitchen/main/static/main/js/recipes.js
+++ inkitchen/main/static/main/js/recipes.js
@@ -1,4 +1,4 @@
-// обработка добавления рецептов в корзину
+// обработка добавления рецептов в корзину и изменения их кол-ва внутри корзины
 $(document).ready(function(){                           // общая обертка для выполнения скрипта после загрузки всего документа html
     $(".btn-add-recipe").click(function(event){         // обработка события по клику на кнопку "+" - добавления блюда в корзину
         event.preventDefault();                         // для того, чтобы страница не перезагружалась при нажатии на кнопку "+"
@@ -9,6 +9,9 @@ $(document).ready(function(){                           // общая оберт
 
         var id = $(this).attr("data-recipe");           // получаем id рецепта, который добавляем в корзину сессии
         data.recipe_id = id;                            // записываем id в данные для отправки на сервер
+
+        var delivery_date = $(this).attr("data-date");   // получаем дату доставки
+        data.delivery_date = delivery_date;             // записываем дату доставки в данные для отправки на сервер
 
         var meal_name = $(this).data("name");           // присваеваем переменной заголовок рецепта
         var meal_image = $(this).data("image");         // присваеваем переменной url фото рецепта
@@ -36,25 +39,104 @@ $(document).ready(function(){                           // общая оберт
                             '<div class="info">' +
                                 '<div class="info-title">' +
                                     '<a href="" class="title text-dark" data-abc="true">'+ meal_name + '</a>'+
-                                    '<form action="{% url ' + "'remove_from_cart'" + id + ' %}">'+
-                                        '<button type="submit" class="btn btn-sm">x</button>'+
-                                    '</form>'+
+                                    '<button type="submit" class="btn-del_from_cart">x</button>'+
                                 '</div>'+
                                 '<div class="meal_count_cart">'+
                                     '<button type="button" onclick="this.nextElementSibling.stepDown()" class="minus">-</button>'+
-                                    '<input type="number" min="0" max="100" value="'+ meal_qty +'" readonly class="raz">'+
+                                    '<input type="number" min="0" max="100" value="'+ meal_qty +'" readonly class="q_portion">'+
                                     '<button type="button" onclick="this.previousElementSibling.stepUp()" class="plus">+</button>'+
                                 '</div>'+
                             '</div>'+
                         '</div>')
                 }else{
                     meal_qty++;
-                    $('#recipe_id_' + id).find('.raz').attr('value', meal_qty);
+                    $('#recipe_id_' + id).find('.q_portion').attr('value', meal_qty);
                 }
             },
 
             error: function(){                           // при ошибке с сервера
-                alert('Error!');
+                alert('Error recipe add!');
+            },
+        })
+    });
+
+    // добавление кол-ва порций внутри корзины
+    $("button.plus").click(function(event){
+        event.preventDefault();
+        var data = {};
+
+        var id = $(this).attr("data-recipe");           // получаем id рецепта, кол-во которого увеличиваем
+        data.recipe_id = id;
+
+        var delivery_date = $(this).attr("data-date");  // получаем дату доставки
+        data.delivery_date = delivery_date;
+
+        var quantity_plus = true;                       // переменная, обозначающая именно увеличение кол-ва
+        data.quantity_plus = quantity_plus;
+
+        var quantity = $("input.q_portion").val();      // получаем текущее кол-во порций в корзине со страницы рецептов
+        data.quantity = quantity;
+
+        $.ajax({
+            url: '../../cart/adjust',                         // url для отправки данных в adjust_cart
+            data: data,
+            success: function(response){
+                console.log('больше')
+            },
+            error: function(){
+                alert('Error quantity plus!');
+            },
+        })
+    });
+
+    // уменьшение кол-ва порций внутри корзины
+    $("button.minus").click(function(event){
+        event.preventDefault();
+        var data = {};
+
+        var id = $(this).attr("data-recipe");           // получаем id рецепта, кол-во которого уменьшаем
+        data.recipe_id = id;
+
+        var delivery_date = $(this).attr("data-date");  // получаем дату доставки
+        data.delivery_date = delivery_date;
+
+        var quantity_minus = true;                      // переменная, обозначающая именно уменьшение кол-ва
+        data.quantity_minus = quantity_minus;
+
+        var quantity = $("input.q_portion").val();      // получаем текущее кол-во порций в корзине со страницы рецептов
+        data.quantity = quantity;
+
+        $.ajax({
+            url: '../../cart/adjust',                         // url для отправки данных в adjust_cart
+            data: data,
+            success: function(response){
+                console.log('меньше')
+            },
+            error: function(){
+                alert('Error quantity minus!');
+            },
+        })
+    });
+    // удаление рецепта из корзины при нажатии на "х"
+    $(".btn-del_from_cart").click(function(event){
+        event.preventDefault();
+        var data = {};
+
+        var id = $(this).attr("data-recipe");           // получаем id рецепта, кол-во которого уменьшаем
+        data.recipe_id = id;
+
+        var delivery_date = $(this).attr("data-date");  // получаем дату доставки
+        data.delivery_date = delivery_date;
+
+        $.ajax({
+            url: '../../cart/remove',                   // url для отправки данных в remove_from_cart
+            data: data,
+            success: function(response){
+                console.log('рецепт удален')
+                $("#recipe_id_" + id).remove();         // удалить рецепт из корзины (DOM-дерева)
+            },
+            error: function(){
+                alert('Error remove recipe!');
             },
         })
     });
