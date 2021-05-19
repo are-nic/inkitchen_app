6146c802fd9606580f2a1d456fd17d0069fc30e2
repair-cat <simf61@yandex.ru>
2021-05-19 13:57:27 +0500

    добавлен роут для вывода страницы о сохранении заказа

--- inkitchen/order/urls.py
+++ inkitchen/order/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import order_create, get_current_location
+from .views import order_create, get_current_location, order_created
 
 urlpatterns = [
     path('create', order_create, name='order_create'),
+    path('created', order_created, name='order_created'),
     path('get_current_location', get_current_location, name='get_current_location'),
 ]
