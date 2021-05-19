9c239e428023218c404358f2c1c893f8467746af
repair-cat <simf61@yandex.ru>
2021-05-19 13:47:36 +0500

    изменен способ сохранения добавленных в корзину блюд

--- inkitchen/cart/contexts.py
+++ inkitchen/cart/contexts.py
@@ -6,26 +6,27 @@ from food.models import Recipe
 
 def cart_contents(request):
     """
-    Подсчет стоимости рецептов и количества их порций.
-    Общая стоимость корзины складывается из стоимости всех рецептов в корзине и количества их порций.
-    Обеспечивает доступность кол-ва порций блюд в корзине в для navbar (не используется)
+    Подсчет стоимости рецептов и кол-ва их порций, добавленных в корзину.
+    Общая стоимость корзины складывается из стоимости всех рецептов в корзине и кол-ва их порций.
     """
     cart = request.session.get('cart', {})
 
-    cart_items = []         # список данных по каждому рецепту (id, q_portion, recipe)
+    cart_items = {}         # список данных по каждому рецепту (id, q_portion, recipe)
     total = 0               # общая стоимость всего заказа
-    recipe_count = 0        # общее количество рецептов
+    recipe_count = 0        # общее количество рецептов в корзине
 
     for delivery_date in cart:                                      # перебираем все даты заказа в корзине
+        cart_items[delivery_date] = []
+        recipe_count += len(cart[delivery_date])                    # прибавляем ко-во блюд по каждой дате заказа
         for recipe_id, q_portion in cart[delivery_date].items():    # перебираем в каждой дате
             recipe = get_object_or_404(Recipe, pk=recipe_id)        # получаем экземпляр рецепта, добавленый в корзину
             total += q_portion * recipe.price                       # общая цена заказа
-            recipe_count += q_portion                               # кол-во в навбаре в пункте Корзина (не исп.)
-            cart_items.append({
+            cart_items[delivery_date].append({
                 'id': recipe_id,
-                'q_portion': q_portion,
                 'recipe': recipe,
+                'q_portion': q_portion,
             })
-            print(cart_items)
-    
+    print('-------------------------------------------------------------------------------')
+    for data in cart_items:
+        print(data, cart_items[data])
     return {'cart_items': cart_items, 'total': total, 'recipe_count': recipe_count}
