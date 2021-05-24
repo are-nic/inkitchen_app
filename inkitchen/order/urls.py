a288bd85d62b0eca23e93f84bad2e450f66330a8
repair-cat <simf61@yandex.ru>
2021-05-24 09:23:38 +0500

    удален роут

--- inkitchen/order/urls.py
+++ inkitchen/order/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import order_create, get_current_location, order_created
+from .views import order_create, get_current_location
 
 urlpatterns = [
     path('create', order_create, name='order_create'),
-    path('created', order_created, name='order_created'),
     path('get_current_location', get_current_location, name='get_current_location'),
 ]
