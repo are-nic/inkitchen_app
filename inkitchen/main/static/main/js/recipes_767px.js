e1fcfec7d4fd6412bca54c9fb11f5f3a9ee411d7
repair-cat <simf61@yandex.ru>
2021-05-25 18:31:09 +0500

    убраны пробелы

--- inkitchen/main/static/main/js/recipes_767px.js
+++ inkitchen/main/static/main/js/recipes_767px.js
@@ -3,20 +3,20 @@
 
 $(document).ready(function() {
     $(window).on('load resize', function(){
-        if ($(window).width() < 768){                           // если размеры экрана меньше чем 768px
+        if ($(window).width() < 768){                               // если размеры экрана меньше чем 768px
 
-            $('#cartModal').append($('#cart_recipes_page_wrap'));  // переносим блок корзины в модальное окно
-            $('#cart_lower').removeAttr('hidden');                 // удаляем атрубут скрытности блока. появляется мобильная корзина
-            $('button.cartLinkModal').click( function(event){
+            $('#cartModal').append($('#cart_recipes_page_wrap'));   // переносим блок корзины в модальное окно
+            $('#cart_lower').removeAttr('hidden');                  // удаляем атрубут скрытности блока. появляется мобильная корзина
+            $('button.cartLinkModal').click(function(event){
                 event.preventDefault();
-                $('#cartOverlay').fadeIn(297,	function(){
+                $('#cartOverlay').fadeIn(297, function(){
                     $('#cartModal')
                     .css('display', 'block')
                     .animate({opacity: 1}, 198);
                 });
             });
 
-            $('#cartModal__close, #cartOverlay').click( function(){
+            $('#cartModal__close, #cartOverlay').click(function(){
                 $('#cartModal').animate({opacity: 0}, 198, function(){
                     $(this).css('display', 'none');
                     $('#cartOverlay').fadeOut(297);
