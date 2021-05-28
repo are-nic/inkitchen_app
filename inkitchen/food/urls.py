34327993d231a2de066cc1f51030dc7cec1f500a
repair-cat <simf61@yandex.ru>
2021-05-28 12:51:43 +0500

    добавлен роут для функции отправки данных об ингрединетах рецепта

--- inkitchen/food/urls.py
+++ inkitchen/food/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import all_recipes, create_recipe, RecipeDetailView, edit_recipe, remove_recipe, delivery_day
+from .views import all_recipes, create_recipe, RecipeDetailView, edit_recipe, remove_recipe, delivery_day, get_ingredients
 
 
 urlpatterns = [
     path('', all_recipes, name='recipes'),
+    path('ingredients', get_ingredients, name='get_ingredients'),
     path('week/<slug:index>', delivery_day, name='delivery_day'),
     path('create', create_recipe, name='create_recipe'),
     path('edit/<id>', edit_recipe, name='edit_recipe'),
