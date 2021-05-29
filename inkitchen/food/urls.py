b8829ee2b84ba4f9485b27a1d79a260170ba8f12
repair-cat <simf61@yandex.ru>
2021-05-29 21:46:03 +0500

    закомментирован роут вывода деталей рецепта

--- inkitchen/food/urls.py
+++ inkitchen/food/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import all_recipes, create_recipe, RecipeDetailView, edit_recipe, remove_recipe, delivery_day, get_ingredients
+from .views import all_recipes, create_recipe, edit_recipe, remove_recipe, delivery_day, get_ingredients
 
 
 urlpatterns = [
@@ -9,5 +9,5 @@ urlpatterns = [
     path('create', create_recipe, name='create_recipe'),
     path('edit/<id>', edit_recipe, name='edit_recipe'),
     path('remove/<id>', remove_recipe, name='remove_recipe'),
-    path('<slug:slug>', RecipeDetailView.as_view(), name='recipe_details'),
+    # path('<slug:slug>', RecipeDetailView.as_view(), name='recipe_details'),
 ]
