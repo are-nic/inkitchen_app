419ca5c82d70f09a7d7417c85feeddd399e767fe
repair-cat <simf61@yandex.ru>
2021-07-19 07:12:35 +0500

    удалена вьюха add_cart и обновлена adjust_cart

--- inkitchen/cart/views.py
+++ inkitchen/cart/views.py
@@ -1,20 +1,6 @@
-from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-
-
-def cart_add(request, id):  # УБРАТЬ
-    """
-    Добавление блюда в корзину
-    """
-    cart = request.session.get('cart', {})
-    quantity = 1  # добавляем одну порцию блюда в корзину
-    cart[id] = cart.get(id, quantity)
-
-    request.session['cart'] = cart
-
-    return redirect(reverse('recipes'))  # после добавления рецепта в корзину возврат к списку рецептов
 
 
 def add_to_cart(request):
@@ -34,34 +20,38 @@ def add_to_cart(request):
         quantity = 1
         cart[delivery_date][recipe_id] = quantity           # заносим в сессию кол-во порций данного блюда
     else:                                                   # если на момент добавления блюда, оно уже в корзине
-        quantity = cart[delivery_date][recipe_id] + 1       # увеличиваем имеющееся кол-во порций юлюда в корзине на 1
+        quantity = cart[delivery_date][recipe_id] + 1       # увеличиваем имеющееся кол-во порций блюда в корзине на 1
         cart[delivery_date][recipe_id] = quantity
 
     cart[delivery_date][recipe_id] = cart.get(recipe_id, quantity)
 
     request.session['cart'] = cart                  # обновляем корзину с учетом изменений
     # messages.error(request, 'Корзина полна, замените рецепт или выберите другой план питания')
-    return HttpResponse("рецепт добавлен")
+    return HttpResponse("Recipe was added")
 
 
 def adjust_cart(request):
     """
     Регулировать кол-во блюд внутри корзины
+    Из Ajax получаем данные о содержимом корзины: рецепты, даты доставки, кол-во порций, а так же логическое True
+    той кнопки ( + или - ), которая была нажата в корзине на кол-ве порций рецепта.
+    В зависимости от нажатой кнопки, уведичиваем или уменьшаем кол-во порций рецепта и обновляем корзину в сессии.
     """
     cart = request.session.get('cart', {})
     recipe_id = request.GET.get('recipe_id', None)
     delivery_date = request.GET.get('delivery_date', None)
     quantity = int(request.GET.get('quantity', None))
 
-    if request.GET.get('quantity_plus'):            # если была нажата кнопка "+" - увеличение кол-ва порций
-        cart[delivery_date][recipe_id] += 1         # добавляем 1 к кол-ву порций конкретного рецепта в корзине
-    elif request.GET.get('quantity_minus'):         # если была нажата кнопка "-" - уменьшение кол-ва порций
-        cart[delivery_date][recipe_id] -= 1         # отнимаем 1 от кол-ва порций конкретного рецепта в корзине
-    elif quantity <= 0:                             # если кол-во порций рецепта в корзине <= 0...
-        cart[delivery_date].pop(recipe_id)          # ...удаляем выбранный рецепт из корзины в сессии
+    # если была нажата кнопка "+" (увеличение кол-ва порций) и текущее кол-во порций рецепта меньше 99
+    if request.GET.get('quantity_plus') and quantity < 99:
+        cart[delivery_date][recipe_id] += 1                 # добавляем 1 к кол-ву порций конкретного рецепта в корзине
+    # если была нажата кнопка "-" (уменьшение кол-ва порций) и текущее кол-во порций рецепта больше 0
+    # Проверка - больше 0, т.к. из ajax приходит текущее (уменьшенное на 1) кол-во порций.
+    elif request.GET.get('quantity_minus') and quantity > 0:
+        cart[delivery_date][recipe_id] -= 1                 # отнимаем 1 от кол-ва порций конкретного рецепта в корзине
 
     request.session['cart'] = cart
-    return HttpResponse("корзина изменена")
+    return HttpResponse("Cart was changed")
 
 
 def remove_from_cart(request):
@@ -79,4 +69,4 @@ def remove_from_cart(request):
     #    cart.pop(delivery_date)             # удаляем эту дату из корзины сессии
 
     request.session['cart'] = cart
-    return HttpResponse("рецепт удален")
+    return HttpResponse("Recipe was removed")
