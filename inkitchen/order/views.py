ecc3e69ddbb1ae97f0e0d9ddd8b976d07f239f27
repair-cat <simf61@yandex.ru>
2021-07-21 04:03:15 +0500

    изменена логика сохранения заказа в БД

--- inkitchen/order/views.py
+++ inkitchen/order/views.py
@@ -4,7 +4,7 @@ from .forms import OrderForm, PlanMenuFormSet
 from .models import OrderRecipe, Order
 from food.models import Recipe
 from django import forms
-from datetime import date, timedelta, time
+from datetime import datetime
 import locale
 
 from geopy.geocoders import Nominatim
@@ -12,7 +12,8 @@ from geopy.geocoders import Nominatim
 
 def order_create(request):
     cart = request.session.get('cart', {})              # получаем содержимое корзины из сессии
-
+    plan_menu = request.session['plan_menu']            # получаем план-меню из сессии
+    delivery_time = ''
     if request.method == 'POST':
         form_order = OrderForm(request.POST)
         if form_order.is_valid():
@@ -20,12 +21,21 @@ def order_create(request):
             order.customer = request.user               # записываем в поле customer текущего пользователя
             order.save()                                # сохраняем в БД экземпляр заказа
 
-            for delivery_datetime in cart:                 # перебираем все дни заказа в корзине сессии
-                if len(cart[delivery_datetime]) > 0:       # если на день заказа есть блюда (словарь дня заказа не пуст)
-                    for recipe_id, qty in cart[delivery_datetime].items():    # перебираем словарь каждого непустого дня заказа
-                        OrderRecipe(                    # создаем экземпляр рецепта для заказа
+            for delivery_date in cart:                  # перебираем все дни заказа в корзине сессии
+                if len(cart[delivery_date]) > 0:        # если на день заказа есть блюда (словарь дня заказа не пуст)
+                    for data in plan_menu:              # перебираем все данные плана-меню сессии по дням
+                        if plan_menu[data]['delivery_date'] == delivery_date:   # когда находим дату в плане меню
+                            delivery_time = plan_menu[data]['delivery_time']    # забираем из плана-меню время доставки
+
+                            # переводим дату и время в формат datetime вида ДД.ММ.ГГГГ ЧЧ:ММ и...
+                            # ...принудительно завершаем цикл
+                            delivery_datetime = datetime.strptime(delivery_date + ' ' + delivery_time, '%d.%m.%Y %H:%M')
+                            break
+                    # перебираем словарь каждого непустого дня заказа
+                    for recipe_id, qty in cart[delivery_date].items():
+                        OrderRecipe.objects.create(                    # создаем экземпляр рецепта для заказа
                             order=order,
-                            recipe=int(recipe_id),
+                            recipe=Recipe.objects.get(id=recipe_id),
                             price=Recipe.objects.get(id=recipe_id).price,
                             qty=qty,
                             delivery_datetime=delivery_datetime
