5efaa5d48a3a9957cb156da3a3b4e7d83899cc44
repair-cat <simf61@yandex.ru>
2021-07-21 06:06:53 +0500

    скрипт обработки доступности чекбокса "оставить заказ у дверей"

---
+++ inkitchen/main/static/main/js/order_page.js
@@ -0,0 +1,11 @@
+// обработка доступности чекбокса "оставить у дверей" при нажатой radio button "Оплата онлайн"
+
+$(document).ready(function() {
+    $('input[name="pay_method"]').click(function(){
+        if ($('#id_pay_method_1').is(':checked')){
+            $('#id_at_door').prop('disabled', false);
+        } else {
+            $('#id_at_door').prop('disabled', true);
+        }
+    })
+});
