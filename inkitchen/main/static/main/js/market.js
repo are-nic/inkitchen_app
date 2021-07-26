5d43ca4f30e3490298f655863d29717791d7a2d9
repair-cat <simf61@yandex.ru>
2021-07-26 10:35:00 +0500

    скрипт маркета

--- inkitchen/main/static/main/js/market.js
+++ inkitchen/main/static/main/js/market.js
@@ -8,3 +8,36 @@ for (i = 0; i < addBtns.length; i++) {
         console.log('productId:', productId, 'action:', action)
     })
 }
+// открытие и закрытие форм заказа под стрелками
+$(document).ready(function(){
+    $('.check-arrow_address, #title_address_forms').click(function(){
+         $('#content_block_address').slideToggle(100, function(){
+              if ($(this).is(':hidden')) {
+                   $('.check-arrow_address').removeClass('open');
+              } else {
+                   $('.check-arrow_address').addClass('open');
+              }
+         });
+         return false;
+    });
+    $('.check-arrow_pay_method, #title_pay_method_forms').click(function(){
+         $('#content_block_pay_method').slideToggle(100, function(){
+              if ($(this).is(':hidden')) {
+                   $('.check-arrow_pay_method').removeClass('open');
+              } else {
+                   $('.check-arrow_pay_method').addClass('open');
+              }
+         });
+         return false;
+    });
+    $('.check-arrow_promo, #title_promo_forms').click(function(){
+         $('#content_block_promo').slideToggle(100, function(){
+              if ($(this).is(':hidden')) {
+                   $('.check-arrow_promo').removeClass('open');
+              } else {
+                   $('.check-arrow_promo').addClass('open');
+              }
+         });
+         return false;
+    });
+});
